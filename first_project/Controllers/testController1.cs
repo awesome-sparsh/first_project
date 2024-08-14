25f02bb25099c0fcb2d774bcbba38e797352a588
sparsh sethi <sparsh sethi@DESKTOP-GSVQ5QO>
2024-08-14 10:54:39 +0530

    added test controller

---
+++ first_project/Controllers/testController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace first_project.Controllers
+{
+    public class testController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
